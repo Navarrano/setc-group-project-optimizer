8dad57a73cf00e874bd423fc837bfe2633d54e88
Jan Kosecki <jan.kosecki91@gmail.com>
2015-03-05 13:29:02 +0000

    Initial project setup

---
+++ OptimizerServer/src/main/java/cranfield/group/project/airfoil/server/App.java
@@ -0,0 +1,13 @@
+package cranfield.group.project.airfoil.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+		System.out.println("Hello World from Server!");
+    }
+}
