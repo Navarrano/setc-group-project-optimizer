0f9b9ec5b15e6904ad916c34b3e0644cdf39fbb6
Jan Kosecki <jan.kosecki91@gmail.com>
2015-03-06 19:41:59 +0000

    Show window for server side

--- OptimizerServer/src/main/java/cranfield/group/project/airfoil/server/ServerApp.java
+++ OptimizerServer/src/main/java/cranfield/group/project/airfoil/server/ServerApp.java
@@ -1,10 +1,8 @@
 package cranfield.group.project.airfoil.server;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.HashSet;
-import java.util.Set;
+import java.awt.EventQueue;
+
+import cranfield.group.project.airfoil.server.gui.MainFrame;
 
 /**
  * Hello world!
@@ -15,29 +13,14 @@ public class ServerApp {
 	private static final int PORT = 6066;
 
 	public static void main(String[] args) {
-		Set<String> connectedUsers = new HashSet<>();
-		ServerSocket socket = null;
-		try {
-			socket = new ServerSocket(PORT);
-			System.out.println("SERVER : Waiting for client on port "
-					+ socket.getLocalPort() + "...");
-			while (true) {
-				Socket client = socket.accept();
-				System.out.println("SERVER : Just connected to "
-						+ client.getRemoteSocketAddress());
-				new Thread(new MarsServer(client, connectedUsers)).start();
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (socket != null) {
-				try {
-					socket.close();
-				} catch (IOException ex) {
-					ex.printStackTrace();
-				}
-			}
 
-		}
+		EventQueue.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				MainFrame mainFrame = new MainFrame();
+				mainFrame.setVisible(true);
+			}
+		});
 	}
 }
